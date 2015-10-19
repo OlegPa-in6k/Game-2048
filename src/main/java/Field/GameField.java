f1cf4f9309812b8c0341a695fe26599d8a7993bb
OlegPa-in6k <olegpa@interlink-ua.com>
2015-10-19 11:20:30 +0300

    Strategu first

---
+++ src/main/java/Field/GameField.java
@@ -0,0 +1,31 @@
+package Field;
+
+/**
+ * Created by employee on 10/19/15.
+ */
+public class GameField {
+    public static final int FIELD_LENGTH = 4;
+
+    private Cell[][] gameField = new Cell[FIELD_LENGTH][FIELD_LENGTH];
+
+    public void setEmptyBoard() {
+        for (int i = 0; i < FIELD_LENGTH; i++) {
+            for (int j = 0; j < FIELD_LENGTH; j++) {
+                Cell cell = new Cell();
+                cell.setNumber(0);
+                gameField[i][j] = cell;
+            }
+        }
+    }
+
+    public String toString() {
+        String string = "";
+        for (int i = 0; i < FIELD_LENGTH; i++) {
+            for (int j = 0; j < FIELD_LENGTH; j++) {
+                string += "" + gameField[i][j].getNumber() + " ";
+            }
+            string += "\n";
+        }
+        return string;
+    }
+}
---
+++ src/main/java/Field/StaticCellGenerator.java
@@ -0,0 +1,7 @@
+package Field;
+
+/**
+ * Created by employee on 10/19/15.
+ */
+public class StaticCellGenerator {
+}
